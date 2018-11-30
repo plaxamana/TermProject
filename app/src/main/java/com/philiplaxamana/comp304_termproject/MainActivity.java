85d9a6bd7d4c81217ef016daf2da3230053a8527
Philip Laxamana <philip.laxamana@hotmail.com>
2018-11-30 12:20:33 -0500

    Created LoginActivity along with the UI

--- app/src/main/java/com/philiplaxamana/comp304_termproject/MainActivity.java
+++ app/src/main/java/com/philiplaxamana/comp304_termproject/MainActivity.java
@@ -1,13 +1,31 @@
 package com.philiplaxamana.comp304_termproject;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActionBar mActionBar;
+    Intent mIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mActionBar = getSupportActionBar();
+        mActionBar.hide();
+    }
+
+    public void logIn(){
+        mIntent = new Intent(this, LoginActivity.class);
+        startActivity(mIntent);
+    }
+
+    public void signUp(){
+        mIntent = new Intent(this, SignupActivity.class);
+        startActivity(mIntent);
     }
 }
